e8fa41be959d34a3c1292655955058154c715c13
Sai Upadhyayula <subramanyam.sai90@gmail.com>
2019-06-28 20:57:42 +0200

    added some more tests for seeding and changing data

--- src/test/java/com/techie/shoppingstore/NgSpringShoppingStoreApplicationTests.java
+++ src/test/java/com/techie/shoppingstore/NgSpringShoppingStoreApplicationTests.java
@@ -4,16 +4,14 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import com.techie.shoppingstore.exceptions.SpringStoreException;
 import com.techie.shoppingstore.model.Category;
 import com.techie.shoppingstore.model.ElasticSearchProduct;
 import com.techie.shoppingstore.model.Product;
 import com.techie.shoppingstore.model.ProductAttribute;
 import com.techie.shoppingstore.repository.CategoryRepository;
 import com.techie.shoppingstore.repository.ProductRepository;
-import com.techie.shoppingstore.repository.elasticsearch.ProductSearchRepository;
+import com.techie.shoppingstore.repository.ProductSearchRepository;
 import com.techie.shoppingstore.service.ProductMapper;
-import org.elasticsearch.index.query.Operator;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,19 +20,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
-import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.data.util.Pair;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
 import java.util.*;
-import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.toSet;
-import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+import static java.util.stream.Collectors.toList;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -135,60 +128,59 @@ public class NgSpringShoppingStoreApplicationTests {
     }
 
     @Test
-    @Ignore
     public void category() {
         Category category = categoryRepository.findByName("Mobile Phones").orElseThrow(() -> new IllegalArgumentException("Invalid Category"));
-        List<Product> products = productRepository.findByCategory(category);
+        List<ElasticSearchProduct> products = productSearchRepository.findByCategory(category);
         category.setPossibleFacets(Arrays.asList("Brand", "4G", "Fingerprint Recognition", "Battery Capacity",
-                "Battery Type", "Glass Type", "Hybrid SIM Slot", "Internal Storage", "Memory(RAM)", "Operating System",
+                "Battery Type", "Glass Type", "Hybrid SIM Slot", "Internal Storage", "Memory(RAM)", "Operating System Type",
                 "SIM Type", "Primary Camera", "Screen Size (Diagonal)", "Selfie Camera"));
         products.forEach(product -> product.setCategory(category));
         categoryRepository.save(category);
-        productRepository.saveAll(products);
+        productSearchRepository.saveAll(products);
 
         Category tablets = categoryRepository.findByName("Tablets").orElseThrow(() -> new IllegalArgumentException("Invalid Category"));
         List<Product> tabletProducts = productRepository.findByCategory(tablets);
-        tablets.setPossibleFacets(Arrays.asList("Brand", "4G", "Battery Type", "Memory(RAM)", "Operating System", "SIM Type", "Primary Camera"));
+        tablets.setPossibleFacets(Arrays.asList("Brand", "4G", "Battery Type", "Memory(RAM)", "Operating System Type", "SIM Type", "Primary Camera"));
         tabletProducts.forEach(product -> product.setCategory(tablets));
         categoryRepository.save(tablets);
         productRepository.saveAll(tabletProducts);
 
         Category laptops = categoryRepository.findByName("Laptops").orElseThrow(() -> new IllegalArgumentException("Invalid Category"));
-        List<Product> laptopProducts = productRepository.findByCategory(laptops);
+        List<Product> laptopProductsMongo = productRepository.findByCategory(laptops);
         laptops.setPossibleFacets(Arrays.asList("Brand", "Battery Type", "Display Type", "Graphics Card - Brand", "Graphics Card - Sub-Brand", "Hard Drive",
-                "HDMI", "Memory (RAM)", "Hard Drive", "Hard Drive Type", "Operating System Type", "Processor Brand", "Core Type","Touch Screen"));
-        laptopProducts.forEach(product -> product.setCategory(laptops));
+                "HDMI", "Memory (RAM)", "Hard Drive Type", "Operating System Type", "Processor Brand", "Core Type", "Touch Screen"));
+        laptopProductsMongo.forEach(product -> product.setCategory(laptops));
         categoryRepository.save(laptops);
-        productRepository.saveAll(laptopProducts);
+        productRepository.saveAll(laptopProductsMongo);
 
         Category gaming = categoryRepository.findByName("Gaming").orElseThrow(() -> new IllegalArgumentException("Invalid Category"));
-        List<Product> gamingProducts = productRepository.findByCategory(gaming);
+        List<ElasticSearchProduct> gamingProducts = productSearchRepository.findByCategory(gaming);
         gaming.setPossibleFacets(Arrays.asList("Brand", "Console Type", "Display Type", "Internal Storage"));
         gamingProducts.forEach(product -> product.setCategory(gaming));
         categoryRepository.save(gaming);
-        productRepository.saveAll(gamingProducts);
+        productSearchRepository.saveAll(gamingProducts);
 
         Category cameras = categoryRepository.findByName("Cameras").orElseThrow(() -> new IllegalArgumentException("Invalid Category"));
-        List<Product> camerasProducts = productRepository.findByCategory(cameras);
+        List<ElasticSearchProduct> camerasProducts = productSearchRepository.findByCategory(cameras);
         cameras.setPossibleFacets(Arrays.asList("Brand", "Battery Type", "Model", "Wi-Fi", "USB", "Focal Length",
                 "HDMI", "Sensor Type", "Display Type"));
         camerasProducts.forEach(product -> product.setCategory(cameras));
         categoryRepository.save(cameras);
-        productRepository.saveAll(camerasProducts);
+        productSearchRepository.saveAll(camerasProducts);
 
         Category smartWatches = categoryRepository.findByName("Smart Watches").orElseThrow(() -> new IllegalArgumentException("Invalid Category"));
-        List<Product> smartWatchProducts = productRepository.findByCategory(smartWatches);
+        List<ElasticSearchProduct> smartWatchProducts = productSearchRepository.findByCategory(smartWatches);
         smartWatches.setPossibleFacets(Arrays.asList("Brand", "Battery Type", "Display", "Display Type", "Gender", "Watch Shape"));
         smartWatchProducts.forEach(product -> product.setCategory(smartWatches));
         categoryRepository.save(smartWatches);
-        productRepository.saveAll(smartWatchProducts);
+        productSearchRepository.saveAll(smartWatchProducts);
 
         Category headphones = categoryRepository.findByName("Headphones & Headsets").orElseThrow(() -> new IllegalArgumentException("Invalid Category"));
-        List<Product> headphonesAndHeadsets = productRepository.findByCategory(headphones);
+        List<ElasticSearchProduct> headphonesAndHeadsets = productSearchRepository.findByCategory(headphones);
         headphones.setPossibleFacets(Arrays.asList("Brand", "Wireless", "Bluetooth", "USB", "Noise Cancellation", "Microphone Type"));
         headphonesAndHeadsets.forEach(product -> product.setCategory(headphones));
         categoryRepository.save(headphones);
-        productRepository.saveAll(headphonesAndHeadsets);
+        productSearchRepository.saveAll(headphonesAndHeadsets);
     }
 
     @Test
@@ -205,38 +197,20 @@ public class NgSpringShoppingStoreApplicationTests {
 
     @Test
     public void saveProductsToES() {
-//        List<Product> products = productRepository.findAll();
-//
-//        List<ElasticSearchProduct> esProducts = products
-//                .stream()
-//                .map(product -> productMapper.productToESProduct(product)).collect(toList());
-//
-//        productSearchRepository.saveAll(esProducts);
-        Category smartWatches = categoryRepository.findByName("Smart Watches")
-                .orElseThrow(() -> new IllegalArgumentException("Invalid Category"));
+        List<Product> products = productRepository.findAll();
 
-        SearchQuery searchQuery = new NativeSearchQueryBuilder()
-                .withQuery(matchQuery("category.name",smartWatches.getName().toLowerCase()).operator(Operator.AND))
-                .withPageable(new PageRequest(0, 2000))
-                .build();
+        List<ElasticSearchProduct> esProducts = products
+                .stream()
+                .map(product -> productMapper.productToESProduct(product)).collect(toList());
 
-        List<ElasticSearchProduct> elasticSearchProducts = elasticsearchTemplate.queryForList(searchQuery, ElasticSearchProduct.class);
-        Map<String, Set<String>> facetValues = new HashMap<>();
-        Set<String> attributes = new HashSet<>();
-        for (String possibleFacet : smartWatches.getPossibleFacets()) {
-            Set<String> productAttributes = elasticSearchProducts.stream()
-                    .map(product->mapAttribute(product, possibleFacet))
-                    .collect(toSet());
-            attributes.addAll(productAttributes.stream().filter(attribute -> !attribute.isEmpty()).collect(toSet()));
+        productSearchRepository.saveAll(esProducts);
 
-        }
-        facetValues.put(smartWatches.getName(), attributes);
         System.out.println("Products");
     }
 
     private String mapAttribute(ElasticSearchProduct product, String facet) {
         for (ProductAttribute productAttribute : product.getProductAttributeList()) {
-            if(productAttribute.getAttributeName().equals(facet)){
+            if (productAttribute.getAttributeName().equals(facet)) {
                 return productAttribute.getAttributeValue();
             }
         }
