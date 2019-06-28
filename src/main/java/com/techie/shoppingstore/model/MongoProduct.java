976e4f6ef721caf9c22ac8cfa706fd97fa56f9a6
Sai Upadhyayula <subramanyam.sai90@gmail.com>
2019-06-28 20:57:26 +0200

    clean up

--- src/main/java/com/techie/shoppingstore/model/MongoProduct.java
+++
@@ -1,7 +0,0 @@
-package com.techie.shoppingstore.model;
-
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document(collection = "Product")
-public class MongoProduct extends Product {
-}
